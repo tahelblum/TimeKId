bb153d37e70184ec8a9cb6e0dde7821dd69f8dfb
tahelblum <tahelblum@gmail.com>
2026-03-02 23:07:36 +0200

    Create api.ts

---
+++ src/lib/api.ts
@@ -0,0 +1,13 @@
+export const API_URL = 'https://x8ki-letl-twmt.n7.xano.io/api:UgeJ6dlR';
+
+export const API_ENDPOINTS = {
+  AUTH: {
+    LOGIN: '/auth/login',
+    SIGNUP: '/auth/signup',
+  },
+  CHILDREN: {
+    CREATE: '/children/create',
+    LIST: '/children/list',
+    ADD_PARENT: '/children/add-parent',
+  },
+};
