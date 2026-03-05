85e9f44662ba52a38da439943013eaed483a43a2
tahelblum <tahelblum@gmail.com>
2026-03-06 01:52:42 +0200

    Update i18n.ts
    Signed-off-by: tahelblum <tahelblum@gmail.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,10 @@
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
-  const locale = 'he';
   const messages = (await import('./messages/he.json')).default;
 
   return {
-    locale,
+    locale: 'he',
     messages: messages as any,
   };
 });
