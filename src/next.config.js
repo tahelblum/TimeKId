e01eab3827995318e8998abe6caf3a6f8f24ef10
tahelblum <tahelblum@gmail.com>
2026-03-06 02:16:53 +0200

    Update next.config.js
    Signed-off-by: tahelblum <tahelblum@gmail.com>

--- src/next.config.js
+++ src/next.config.js
@@ -1,5 +1,5 @@
 const createNextIntlPlugin = require('next-intl/plugin');
-const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
